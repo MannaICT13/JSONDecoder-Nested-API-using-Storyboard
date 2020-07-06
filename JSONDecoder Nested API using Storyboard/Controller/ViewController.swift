12dbb0b933a3c342f8e6d75c8e7d8b0257f8c439
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-07-06 23:45:45 +0600

    added User cell and outlets

--- JSONDecoder Nested API using Storyboard/Controller/ViewController.swift
+++ JSONDecoder Nested API using Storyboard/Controller/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     //MARK:- Properties
     var users = [User]()
     
+    @IBOutlet weak var tableView: UITableView!
     
     //MARK:- Initializers
     
---
+++ JSONDecoder Nested API using Storyboard/View/UserCell.swift
@@ -0,0 +1,27 @@
+//
+//  UserCell.swift
+//  JSONDecoder Nested API using Storyboard
+//
+//  Created by Md Khaled Hasan Manna on 6/7/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    //MARK:- Properties
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var cityLbl: UILabel!
+    @IBOutlet weak var latitideLbl: UILabel!
+    
+    //MARK:- Initializers
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+
+}
